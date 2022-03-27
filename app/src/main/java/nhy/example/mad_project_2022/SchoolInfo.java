16cb2fe655ca09ad799e665b0203fa7b1c0593b2
ahoia <gordonng382804@gmail.com>
2022-03-27 12:20:44 +0800

    create SchoolInfo.java

---
+++ app/src/main/java/nhy/example/mad_project_2022/SchoolInfo.java
@@ -0,0 +1,28 @@
+package nhy.example.mad_project_2022;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SchoolInfo {
+    public static String NAME = "name";
+    public static String EMAIL = "email";
+    public static String ADDRESS = "address";
+    public static String MOBILE = "mobile";
+
+    public static ArrayList<HashMap<String, String>> schoolList = new ArrayList<>();
+
+    // Creates and add contact to contact list
+    public static void addContact(String name, String email, String address, String mobile) {
+        // Create contact
+        HashMap<String, String> school = new HashMap<>();
+        school.put(NAME, name);
+        school.put(EMAIL, email);
+        school.put(ADDRESS, address);
+        school.put(MOBILE, mobile);
+
+        // Add contact to contact list
+        schoolList.add(school);
+    }
+}
+
+
