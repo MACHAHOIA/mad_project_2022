9b5de703c276bceaf3840c930a140cd96bfa3617
ahoia <gordonng382804@gmail.com>
2022-04-16 21:45:10 +0800

    finish all functions

--- app/src/main/java/nhy/example/mad_project_2022/MainActivity.java
+++ app/src/main/java/nhy/example/mad_project_2022/MainActivity.java
@@ -2,10 +2,16 @@ package nhy.example.mad_project_2022;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,12 +29,23 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     private ListView listView;
     SimpleAdapter adapter;
+    static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        listView = (ListView) findViewById(R.id.listview);
+
+        if (!checkPermission()) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        } else {
+        }
+
+
+
+    listView = (ListView) findViewById(R.id.listview);
         JsonHandlerThread jsonHandlerThread = new JsonHandlerThread();
         jsonHandlerThread.start();
         try {
@@ -57,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                             }});
                         builder.setNegativeButton("Search",new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                dialog.cancel();
+                                showMap(contact.get(SchoolInfo.NAME));
                         }});
                         builder.setNegativeButtonIcon(getDrawable(R.drawable.ic_baseline_map_24));
 
@@ -71,8 +88,42 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
+    }
+
+    public void showMap(String address) {
+        String geo = "http://maps.google.com/maps?q=" + address + ", Hong Kong";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geo));
+        startActivity(intent);
+    }
+
+
+    public boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    String msg = "Please run the app again and grant the required permission.";
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-        @Override
+    }
+
+
+
+    @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             MenuInflater inflater = getMenuInflater();
             inflater.inflate(R.menu.search_menu, menu);
--- app/src/main/java/nhy/example/mad_project_2022/SplashActivity.java
+++ app/src/main/java/nhy/example/mad_project_2022/SplashActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
 
         Handler handler = new Handler();
@@ -21,6 +23,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        }, 3000);
+        }, 1500);
     }
 }
